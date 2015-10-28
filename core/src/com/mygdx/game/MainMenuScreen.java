cf6e7047fb44529a7c45142da8848bf22080ebae
Junta Suzuki <rohei67@gmail.com>
2015-10-28 18:33:15 +0900

    暫定タイトル画面を表示。 Viewportでアスペクト比を維持する処理を実装

---
+++ core/src/com/mygdx/game/MainMenuScreen.java
@@ -0,0 +1,54 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class MainMenuScreen extends ScreenAdapter {
+	Ascend _game;
+	OrthographicCamera _camera;
+	Viewport _viewport;
+
+	Vector3 _touchPoint;
+	SpriteBatch _batch;
+
+	public MainMenuScreen(Ascend game) {
+		this._game = game;
+		_camera = new OrthographicCamera(Ascend.GAME_WIDTH, Ascend.GAME_HEIGHT);
+		_camera.position.set(_camera.viewportWidth / 2, _camera.viewportHeight / 2, 0);
+
+		_viewport = new FitViewport(Ascend.GAME_WIDTH, Ascend.GAME_HEIGHT, _camera);
+		_viewport.apply();
+
+		_batch = new SpriteBatch();
+		_touchPoint = new Vector3();
+	}
+
+	@Override
+	public void render(float delta) {
+		draw();
+	}
+
+	private void draw() {
+		GL20 gl = Gdx.gl;
+		gl.glClearColor(0, 0, 0, 1);
+		gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		_camera.update();
+		_batch.setProjectionMatrix(_camera.combined);
+
+		_batch.begin();
+		_batch.draw(Assets.title, 0, 0);
+		_batch.end();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		_viewport.update(width,height);
+		_camera.position.set(_camera.viewportWidth/2,_camera.viewportHeight/2,0);
+	}
+}
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.mygdx.game.Ascend;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Ascend!";
+		config.width = 480;
+		config.height = 800;
 		new LwjglApplication(new Ascend(), config);
 	}
 }
